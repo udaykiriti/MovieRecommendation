1b733d682a39a6ae531a9fe4b0a856883b0f4810
Uday__ <123718855+udaykiriti@users.noreply.github.com>
2025-07-22 07:16:10 +0530

    Update settings.py

--- imdb_project/settings.py
+++ imdb_project/settings.py
@@ -15,13 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1ry&8q5ua&8eizlh*a1)1*r-fp0vo@yyluj0-)sa7fuio!kd57'
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
